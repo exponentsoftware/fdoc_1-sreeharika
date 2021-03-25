ee9fc765f57a14bbd7801dce2d10b84d7bce1cb5
Sreeharika <sreeharikayelle@gmail.com>
2021-03-25 19:08:25 +0530

    counted words in string

---
+++ answer1c.js
@@ -0,0 +1,6 @@
+const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+function countWords(str){
+   
+return str.split(' ').length;
+}
+console.log(countWords(sentence));
